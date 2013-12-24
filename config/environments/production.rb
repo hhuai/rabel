6c4d78f0654690e798b6b09681faf492044f89c2
hhuai <8863824@gmail.com>
2013-12-24 10:00:58 +0800

    asset to 7niu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rabel::Application.configure do
     }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = Figaro.env.ASSET_HOST
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += [ Proc.new {|path| File.basename(path).start_with?('i_')} ]
