49561b18efac66550a6e835ba40cf40efa82f0d4
hhuai <8863824@gmail.com>
2013-12-25 20:43:03 +0800

    去除i18n warning

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ module Rabel
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml')]
     config.i18n.default_locale = :zh
+    I18n.enforce_available_locales = false 
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
